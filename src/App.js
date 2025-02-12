c005ccc93e866b07f662e345790200885695c0a4
DongYoon Kim <77170214+BusanGukbap@users.noreply.github.com>
2025-02-12 11:20:27 +0900

    feat: FE_반짝 예메 화면

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const handleClick = () => {
+    alert("반짝 반짝");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="phone-container">
+      <div className="phone-frame">
+        <div className="notch"></div>
+        <div className="phone-screen">
+          {/* 로고 이미지: public 경로 참조 시 /images/... 로 시작 */}
+          <img
+            src="/images/olive_young_logo.png"
+            className="phone-logo"
+            alt="Olive Young Logo"
+          />
+          <button className="reserve-btn noto-sans-kr" onClick={handleClick}>
+            반짝 예매
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
