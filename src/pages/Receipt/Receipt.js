0a5ff5f19c0049c3598d30709265ea81e92a4e35
takedown <dong2857210@gmail.com>
2025-02-24 04:36:01 +0900

    design : 올영 로고 삭제

--- src/pages/Receipt/Receipt.js
+++ src/pages/Receipt/Receipt.js
@@ -12,14 +12,7 @@ function Receipt() {
   };
 
   return (
-    <PhoneFrame>{/* 로고 영역 */}
-      <div className="logo-container">
-        <img
-          src="/images/olive_young_logo.png"
-          className="phone-logo"
-          alt="Olive Young Logo"
-        />
-      </div>
+    <PhoneFrame>
       {/* 중앙 영역 */}
       <div className="center-content noto-sans-kr">
         <h2>예매가 완료되었습니다!</h2>
